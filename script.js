1aded272aef5b3130ce3fc85453c3df5b71819ea
Mahima Makode <140474655+MahimaMakode@users.noreply.github.com>
2023-08-18 17:41:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,59 +32,12 @@ gsap.from("#main .img1",{
         end:"bottom 0%",
     }
 })
-// gsap.from("#equipment .img4",{
-//     x:300,
-//     duration:1,
-//     opacity:0,
-//     scrollTrigger:{
-//         trigger:"#equipment .img4",
-//         scroller:"body",
-//         scrub:2,
-//         // markers:true,
-//         start:"top 50%",
-//         end:"top 60%",
-//     }
-// })
-// gsap.from("#equipment1 .img5",{
-//     x:-300,
-//     duration:1,
-//     opacity:0,
-//     scrollTrigger:{
-//         trigger:"#equipment1 .img5",
-//         scroller:"body",
-//         scrub:2,
-//         // markers:true,
-//         start:"top 50%",
-//         end:"top 60%",
-//     }
-// })
-// gsap.from("#equipment2 .img4",{
-//     x:300,
-//     duration:1,
-//     opacity:0,
-//     scrollTrigger:{
-//         trigger:"#equipment2 .img4",
-//         scroller:"body",
-//         scrub:2,
-//         // markers:true,
-//         start:"top 50%",
-//         end:"top 60%",
-//     }
-// })
+
 gsap.from("#main .contain h4",{
     y:-20,
     duration:.7,
     yoyo:-1,
     repeat:-1,
     
-
-    // opacity:0,
-    // scrollTrigger:{
-        // trigger:"#main .contain h4",
-        // scroller:"body",
-        // scrub:2,
-        // markers:true,
-        // start:"top 50%",
-        // end:"top 60%",
     
 })
